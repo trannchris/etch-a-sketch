8935ccce7aee5e6654ee1d4cac13c5b7625ee180
Chris Tran <60266790+trannchris@users.noreply.github.com>
2023-10-06 18:36:50 -0500

    Add size changing functionality

--- app.js
+++ app.js
@@ -11,25 +11,32 @@ createGrid = () => {
   }
 };
 
-changeGridSize = () => {
-  gridSize = prompt("Please enter a size between 2 and 100.");
-  if (Number.isNaN(gridSize) || 2 < gridSize > 100) {
-    alert("Invalid grid size");
-  } else {
-    removeAllChildNodes(grid);
-    grid.setAttribute(
-      "style",
-      `grid-template-columns: repeat(${gridSize}, 2fr); grid-template-rows: repeat(${gridSize}, 2fr)`
-    );
-    for (let i = 0; i < gridSize ** 2; i++) {
-      const gridCell = document.createElement("div");
-      gridCell.classList.add("gridCell");
-      gridCell.addEventListener("mouseover", function (event) {
-        event.target.style.backgroundColor = "black";
-      });
-      grid.appendChild(gridCell);
-    }
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
   }
-};
+}
+
+const slider = document.querySelector("#slider");
+const sizeValue = document.querySelector(".value");
+slider.addEventListener("input", function () {
+  let size = document.querySelector("#slider").value;
+  sizeValue.textContent = size;
+  removeAllChildNodes(grid);
+  grid.setAttribute(
+    "style",
+    `grid-template-columns: repeat(${size}, 2fr); grid-template-rows: repeat(${size}, 2fr)`
+  );
+  for (let i = 0; i < size ** 2; i++) {
+    const gridCell = document.createElement("div");
+    gridCell.classList.add("gridCell");
+    gridCell.addEventListener("mouseover", function (event) {
+      event.target.style.backgroundColor = "black";
+    });
+    grid.appendChild(gridCell);
+  }
+});
+
+resetGrid = () => {};
 
 createGrid();
